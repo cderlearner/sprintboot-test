c80ef4baff2623d84eee06c259497b0582e2b917
linjiaxing <linjiaxing@zhihu.com>
2019-04-01 10:06:49 +0800

    fix

--- src/main/java/com/example/sptest/bean/other/Child.java
+++ src/main/java/com/example/sptest/bean/other/Child.java
@@ -1,5 +1,8 @@
 package com.example.sptest.bean.other;
 
+import com.example.sptest.bean.dto.CanOpenRequest;
+import com.example.sptest.bean.dto.Pojo;
+
 /**
  * Author: linjx
  * Date: 2019/3/31
@@ -11,8 +14,19 @@ public class Child extends Base{
         System.out.println("hello world !");
     }
 
+
+    public static void pojo(Pojo pojo) {
+        //CanOpenRequest req = (CanOpenRequest) pojo;
+        System.out.println(pojo);
+    }
+
     public static void main(String[] args) {
         Base base = new Child();
         base.hi();
+
+        CanOpenRequest canOpenRequest = new CanOpenRequest();
+        canOpenRequest.setMobile("128");
+        canOpenRequest.setAppId("111");
+        pojo(canOpenRequest);
     }
 }
